98fe4165fabe3239dfa1aca52fc813c533bfb7bb
Lewis England <lewis2004@hotmail.com>
2023-06-27 21:43:44 +0100

    feat: add endpoints for project atttach, suspend and resume

--- src/boinc_api/blueprints/v1/projects.py
+++ src/boinc_api/blueprints/v1/projects.py
@@ -7,3 +7,35 @@ blueprint = Blueprint(name="projects", import_name=__name__, url_prefix="/projec
 def get_all_projects():
     client_id = request.args["client"]
     return current_app.config["clients"][client_id]["instance"].get_all_projects()
+
+
+@blueprint.route("/attach", methods=["POST"])
+def attach_to_project():
+    client_id = request.args["client"]
+    req_json = request.json
+
+    return current_app.config["clients"][client_id]["instance"].attach_project(
+        name=req_json["name"],
+        url=req_json["url"],
+        key=req_json["key"],
+    )
+
+
+@blueprint.route("/suspend", methods=["POST"])
+def suspend_project():
+    client_id = request.args["client"]
+    req_json = request.json
+
+    return current_app.config["clients"][client_id]["instance"].suspend_project(
+        url=req_json["url"],
+    )
+
+
+@blueprint.route("/resume", methods=["POST"])
+def resume_project():
+    client_id = request.args["client"]
+    req_json = request.json
+
+    return current_app.config["clients"][client_id]["instance"].resume_project(
+        url=req_json["url"],
+    )
--- tests/unit/v1/test_projects.py
+++ tests/unit/v1/test_projects.py
@@ -3,3 +3,35 @@ def test_can_get_list_of_projects(mock_client):
 
     assert resp.status_code == 200
     assert len(resp.json["projects"]) == 1
+
+
+def test_can_start_the_project_attachment_process(mock_client):
+    resp = mock_client.post(
+        "/v1/projects/attach?client=1",
+        json={
+            "name": "Test Project",
+            "url": "https://testproject.org",
+            "key": "token_123",
+        },
+    )
+
+    assert resp.status_code == 200
+    assert resp.json == {"success": "true"}
+
+
+def test_can_suspend_project(mock_client):
+    resp = mock_client.post(
+        "/v1/projects/suspend?client=1", json={"url": "https://testproject.org"}
+    )
+
+    assert resp.status_code == 200
+    assert resp.json == {"success": "true"}
+
+
+def test_can_resume_project(mock_client):
+    resp = mock_client.post(
+        "/v1/projects/resume?client=1", json={"url": "https://testproject.org"}
+    )
+
+    assert resp.status_code == 200
+    assert resp.json == {"success": "true"}
